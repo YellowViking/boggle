787dec51559b6899f662f55c7298cd4efb6c71f3
Buyu Chen <chenx798@umn.edu>
2019-01-16 10:11:59 -0800

    ServerSide Milestone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,53 @@
+# frozen_string_literal: true
+
+require 'set'
 class ApplicationController < ActionController::API
+  BOARD_SIZE = 4
+
+  def game
+    alphabet = ('a'..'z').to_a
+    alphabet.delete('u')
+    board = (1..BOARD_SIZE).map {(1..BOARD_SIZE).map {alphabet.at(rand(alphabet.size))}}
+    render json: board, status: :ok
+  end
+
+
+  def validate
+    board = JSON.parse params[:board]
+    word = params[:word]
+
+    result = false
+    visited = Set.new
+
+    search = lambda {|i, j, index|
+      return false if i < 0 or j < 0 or
+          (i == BOARD_SIZE) or (j == BOARD_SIZE)
+      return true if index == word.size
+      return false if board[i][j] != word[index]
+
+      visit_key = "#{i}-#{j}"
+      return false if visited.include? visit_key
+
+      visited.add visit_key
+      any_match = (i - 1..i + 1).any? do |next_i|
+        (j - 1..j + 1).any? do |next_j|
+          search[next_i, next_j, index + 1]
+        end
+      end
+      visited.delete visit_key
+      any_match
+    }
+
+    try_search = lambda {
+      (0..3).any? do |i|
+        (0..3).any? do |j|
+          search[i, j, 0]
+        end
+      end
+    }
+
+    result = try_search[] if word.size > 2
+
+    render json: result, status: :ok
+  end
 end
---
+++ test/controllers/application_controller_test.rb
@@ -0,0 +1,57 @@
+require 'test_helper'
+class ApplicationControllerTest < ActionDispatch::IntegrationTest
+  test "the truth" do
+    assert true
+  end
+
+  test "get /game should generate board" do
+    get '/game'
+    board = JSON.parse(@response.body)
+    assert_equal 4, board.size
+    board.each {|row| assert_equal 4, row.size}
+  end
+
+  test "get board should return different boards" do
+    get '/game'
+    board1 = JSON.parse(@response.body)
+    get '/game'
+    board2 = JSON.parse(@response.body)
+    assert_not_equal board1.flatten, board2.flatten
+  end
+
+  test 'validate returns false when word length too small' do
+    post '/validate', params: {
+        board: [
+            %w[a b c d],
+            %w[a b c d],
+            %w[a b c d],
+            %w[a b c d]].to_json,
+        word: 'aa'}
+    result = JSON.parse(@response.body)
+    assert_equal false, result
+  end
+
+  test 'validate returns true when word is found' do
+    post '/validate', params: {
+        board: [
+            %w[a b c d],
+            %w[a b c d],
+            %w[a b c d],
+            %w[a b c d]].to_json,
+        word: 'aaa'}
+    result = JSON.parse(@response.body)
+    assert_equal true, result
+  end
+
+  test 'validate returns false when cell is visited' do
+    post '/validate', params: {
+        board: [
+            %w[a b c d],
+            %w[a b c d],
+            %w[a b c d],
+            %w[a b c d]].to_json,
+        word: 'aaaaa'}
+    result = JSON.parse(@response.body)
+    assert_equal false, result
+  end
+end
